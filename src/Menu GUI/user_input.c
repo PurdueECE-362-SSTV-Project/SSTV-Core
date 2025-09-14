08aa716c243aff8353013ce3fe73140cf7bbbf73
AaryanPanigrahi <aaryan.panigrahii@gmail.com>
2025-09-14 14:10:42 -0400

    Setting up

---
+++ src/Menu GUI/user_input.c
@@ -0,0 +1,12 @@
+////    ////    ////    ////
+// LIBRARIES //
+////    ////    ////    ////
+// ** // ** // ** //
+// Core Libraries
+#include <stdio.h>
+#include "pico.h"
+
+// Touchscreen Libraries - SPI
+#include "../lib/Adafruit-GFX-Library-1.12.1/Adafruit_GFX.h"        
+#include "../lib/TFTLCD-Library-1.0.3/Adafruit_TFTLCD.h"  
+#include "../lib/Adafruit_TouchScreen-1.1.6/TouchScreen.h"

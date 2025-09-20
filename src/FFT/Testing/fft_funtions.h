c7ea93e70e6c43df1857f4ef88e14fa8f978e854
AaryanPanigrahi <aaryan.panigrahii@gmail.com>
2025-09-20 15:37:10 -0400

    fft setup

--- src/FFT/Testing/fft_funtions.h
+++ src/FFT/Testing/fft_funtions.h
@@ -1,12 +1,11 @@
 #ifndef _FFT_FUNTIONS
 #define _FFT_FUNTIONS
 
-#include "../lib/kissfft"
+#include "../lib/kissfft-master/kiss_fft.h"
+#include "../lib/kissfft-master/kiss_fftr.h"
 #include <stdlib.h>
 #include <stdio.h>
 
-#define SAMPLING_FREQUENCY  11000   // 11 
-
-
+#define SAMPLING_FREQUENCY  11000   // 11 k
 
 #endif
--- src/FFT/Testing/fft_test.c
+++ src/FFT/Testing/fft_test.c
@@ -1,4 +1,34 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>  // for usleep()
 
+#include "fft_funtions.h"
+
+#define NFFT 1024
+#define SRATE 44100
+
+int main() {
+    // Allocate
+    double *in_r = malloc(sizeof(double)*NFFT);
+    kiss_fft_cpx *out_c = malloc(sizeof(kiss_fft_cpx)*(NFFT/2+1));
+    kiss_fft_cfg cfg = kiss_fftr_alloc(NFFT, 0, 0, 0);
+
+    // Fill input (e.g., from audio capture)
+    for (int i = 0; i < NFFT; i++)
+        in_r[i] = /* your real samples */ 0.0;
+
+    // Execute FFT
+    kiss_fftr(cfg, in_r, out_c);
+
+    // Process spectrum
+    for (int k = 0; k <= NFFT/2; k++) {
+        double freq = k * (double)SRATE / NFFT;
+        double mag  = sqrt(out_c[k].r*out_c[k].r + out_c[k].i*out_c[k].i);
+        printf("%6.1f Hz: %f\n", freq, mag);
+    }
+
+    // Cleanup
+    kiss_fft_free(cfg);
+    free(in_r);
+    free(out_c);
+    return 0;
+}

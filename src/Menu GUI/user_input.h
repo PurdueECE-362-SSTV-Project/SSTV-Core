303ede2d58c61c4d99ffeeada0e01218147742cb
AaryanPanigrahi <aaryan.panigrahii@gmail.com>
2025-09-20 13:59:34 -0400

    fft setup

--- src/Menu GUI/user_input.h
+++ src/Menu GUI/user_input.h
@@ -10,9 +10,6 @@
 #include "pico/stdlib.h"
 
 // Touchscreen Libraries - SPI
-#include "../lib/Adafruit-GFX-Library-1.12.1/Adafruit_GFX.h"        
-#include "../lib/TFTLCD-Library-1.0.3/Adafruit_TFTLCD.h"  
-#include "../lib/Adafruit_TouchScreen-1.1.6/TouchScreen.h"
 
 ////    ////    ////    ////
 // Global Definations //

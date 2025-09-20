9c64be7d9d068ade185756b74fddba8e8c8960ef
Aaryan Panigrahi <panigraa@ecelnx61.ecn.purdue.edu>
2025-09-20 18:06:05 -0400

    fft input 2

--- src/FFT/Testing/fft_input_aaryan.c
+++ src/FFT/Testing/fft_input_aaryan.c
@@ -3,40 +3,14 @@
 
 #include "fft_funtions.h"
 
-// TIME DURATION LIST
-#define TIME_VIS_SYNC_1      300
-#define TIME_VIS_SYNC_HOLD   10 + TIME_VIS_SYNC_1
-#define TIME_VIS_SYNC_2      300 + TIME_VIS_SYNC_HOLD
-
-#define TIME_START_BIT      30 + TIME_VIS_SYNC_2
-#define TIME_VIS_CODE       30 * 8 + TIME_START_BIT
-#define TIME_PARITY_BIT     30 + TIME_VIS_CODE
-#define TIME_STOP_BIT       30 + TIME_PARITY_BIT
-
-#define TIME_TRANSMISSION   1000 + TIME_STOP_BIT
-
-// FREQUENCY LIST
-#define FREQ_VIS_SYNC_1         1900
-#define FREQ_VIS_SYNC_HOLD      1200 
-#define FREQ_VIS_SYNC_2         1900
-
-#define FREQ_START_BIT          1200
-#define FREQ_VIS_HIGH           1300 
-#define FREQ_VIS_LOW            1100 
-#define FREQ_STOP_BIT           1200
-
-#define FREQ_TRANSMISSION       1000
-
 uint32_t time_global = 0;
 
-static double gen_sin(uint32_t frequency) {
-        return sin(time_global * frequency);
-}
+static double gen_sin(uint32_t frequency)   return sin(time_global * frequency);
 
 // // fft_input_custom_gen //
 // time: Current Time (x(t))
 // start_time: Where the SSTV signal starts
-double fft_input_custom_gen (uint32_t time, uint32_t start_time) {
+double fft_input_custom_gen (uint32_t time, uint32_t start_time) { 
     // ret value
     double output_val;
 
@@ -44,8 +18,16 @@ double fft_input_custom_gen (uint32_t time, uint32_t start_time) {
     uint32_t time_n = time - start_time;
     time_global = time;
     
-    if (time_n > 0)         output_val = gen_sin(1);
-    else if (TIME_BOUNDS(time_n, 0, TIME_VIS_SYNC_1))                    output_val = gen_sin(FREQ_VIS_SYNC_1);
-    else if (TIME_BOUNDS(time_n, TIME_VIS_SYNC_1, TIME_VIS_SYNC_HOLD))   output_val = gen_sin(FREQ_VIS_SYNC_HOLD);
-    else if (TIME_BOUNDS(time_n, TIME_VIS_SYNC_HOLD, TIME_VIS_SYNC_2))   output_val = gen_sin(FREQ_VIS_SYNC_1);
+    if (time_n > 0) output_val = gen_sin(1);
+    else if (TIME_BOUNDS(time_n, 0, TIME_VIS_SYNC_1))                       output_val = gen_sin(FREQ_VIS_SYNC_1);
+    else if (TIME_BOUNDS(time_n, TIME_VIS_SYNC_1, TIME_VIS_SYNC_HOLD))      output_val = gen_sin(FREQ_VIS_SYNC_HOLD);
+    else if (TIME_BOUNDS(time_n, TIME_VIS_SYNC_HOLD, TIME_VIS_SYNC_2))      output_val = gen_sin(FREQ_VIS_SYNC_1);
+    else if (TIME_BOUNDS(time_n, TIME_VIS_SYNC_2, TIME_START_BIT))          output_val = gen_sin(FREQ_START_BIT);
+    else if (TIME_BOUNDS(time_n, TIME_START_BIT, TIME_VIS_CODE))            output_val = gen_sin(FREQ_VIS_HIGH);
+    else if (TIME_BOUNDS(time_n, TIME_VIS_CODE, TIME_PARITY_BIT))           output_val = gen_sin(FREQ_VIS_LOW);
+    else if (TIME_BOUNDS(time_n, TIME_PARITY_BIT, TIME_STOP_BIT))           output_val = gen_sin(FREQ_STOP_BIT);
+    else if (TIME_BOUNDS(time_n, TIME_STOP_BIT, TIME_TRANSMISSION))         output_val = gen_sin(FREQ_TRANSMISSION);
+    else     output_val = 0.0;                  // fallback or silence outside defined time range
+
+    return output_val;
 } 

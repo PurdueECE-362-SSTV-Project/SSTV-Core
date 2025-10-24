ea8d42c9d3ac4b2aa3c010a89a0d461c63765796
Andrew Thomas Choung <achoung@ecelnx63.ecn.purdue.edu>
2025-10-24 17:26:18 -0400

    Decoder files

--- src/decoder.c
+++ src/decoder.c
@@ -7,6 +7,73 @@
 
 #include "decoder.h"
 
+void header() {
+    Header_State state = IDLE;
+    Header_State nextState;
+    Decoder_FSM_Val in;
+    uint16_t VIS_Code;
+    uint bit;
+    uint8_t codeSum; // Used to compute parity
+
+    switch (state) {
+        case (SYNC1):
+            if (in.freq == h_sync1.freq && in.diff_time == h_sync1.diff_time) nextState = SYNC_HOLD;
+            else nextState = state;
+            break;
+
+        case (SYNC_HOLD):
+            if (in.freq == h_hold.freq && in.diff_time == h_hold.diff_time) nextState = SYNC2;
+            else if (in.freq != h_hold.freq || in.diff_time > h_hold.diff_time) nextState = TRANSMISSION_ERROR;
+            break;
+
+        case (SYNC2):
+            if (in.freq == h_sync2.freq && in.diff_time == h_sync2.diff_time) nextState = START_BIT;
+            else if (in.freq != h_sync2.freq || in.diff_time > h_sync2.diff_time) nextState = TRANSMISSION_ERROR;
+            break;
+
+        case (START_BIT):
+            if (in.freq == h_start.freq && in.diff_time == h_start.diff_time) nextState = CODE;
+            else if (in.freq != h_start.freq || in.diff_time > h_start.diff_time) nextState = TRANSMISSION_ERROR;
+            break;
+
+        case (CODE):
+            if (in.freq <= h_code.freq && in.freq >= h_code.freq_lb && in.diff_time == h_code.diff_time) nextState = PARITY_BIT;
+            else if (in.freq != h_code.freq || in.diff_time > h_code.diff_time) nextState = TRANSMISSION_ERROR;
+            
+            if (in.freq == 1300) bit = 0; if (in.freq == 1100) bit = 1; // Determine input binary val
+            VIS_Code = (VIS_Code << 1u) + bit; // Add value to code
+            break;
+
+        case(PARITY_BIT):
+            if (in.freq <= h_parity.freq && in.freq >= h_parity.freq_lb  && in.diff_time == h_parity.diff_time) nextState = STOP_BIT;
+            else if (in.freq != h_parity.freq || in.diff_time > h_parity.diff_time) nextState = TRANSMISSION_ERROR;
+
+            uint16_t temp = VIS_Code;
+            if (in.freq == 1300) bit = 0; if (in.freq == 1100) bit = 1; // Determine parity bit
+            for (int i = 0; i < 7; i++) {
+                codeSum += (temp % 10); // Add LSB to sum
+                temp /= 10;             // Shift code right 1 bit
+            }
+            if (codeSum + bit % 2) nextState = TRANSMISSION_ERROR; // Parity mismatch
+            break;
+
+        case(STOP_BIT):
+            if (in.freq == h_stop.freq && in.diff_time == h_stop.diff_time) nextState = SYNC1;
+            else if (in.freq != h_stop.freq || in.diff_time > h_stop.diff_time) nextState = TRANSMISSION_ERROR;
+            break;
+
+        case (TRANSMISSION_ERROR):
+            nextState = SYNC1;
+            printf("Transmission Error");
+            break;
+
+        default:
+            nextState = SYNC1;
+            break;
+    }
+    return;
+}
+
 void decoder() {
     return; 
 }
--- src/decoder.h
+++ src/decoder.h
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdint.h>
 #include "pico/stdlib.h"
 #include "hardware/xosc.h"
 #include "pico/multicore.h"
@@ -31,6 +32,30 @@ typedef enum {
     PD_50     = 1011101,
     PD_90     = 1100011  
  } Mode;
+
+
+ typedef enum {
+    IDLE, SYNC1, SYNC_HOLD, SYNC2, START_BIT, CODE, PARITY_BIT, STOP_BIT, TRANSMISSION_ERROR
+ } Header_State; 
+
+  typedef struct {
+    Header_State curState;
+    uint16_t freq;      // Frequency (upper bound if applicable)
+    uint16_t freq_lb;   // Frequency lower bound
+    uint16_t diff_time; // Difference in time in ms
+    uint16_t code;      // VIS Code
+ } Decoder_FSM_Val;
+
+// Global Constants
+Decoder_FSM_Val h_idle   = {.curState = IDLE,       .freq = NULL, .freq_lb = NULL, .diff_time = NULL};
+Decoder_FSM_Val h_sync1  = {.curState = SYNC1,      .freq = 1900, .freq_lb = NULL, .diff_time = 300};
+Decoder_FSM_Val h_hold   = {.curState = SYNC_HOLD,  .freq = 1200, .freq_lb = NULL, .diff_time = 10};
+Decoder_FSM_Val h_sync2  = {.curState = SYNC2,      .freq = 1900, .freq_lb = NULL, .diff_time = 300};
+Decoder_FSM_Val h_start  = {.curState = START_BIT,  .freq = 1200, .freq_lb = NULL, .diff_time = 30};
+Decoder_FSM_Val h_code   = {.curState = CODE,       .freq = 1300, .freq_lb = 1100, .diff_time = 240};
+Decoder_FSM_Val h_parity = {.curState = PARITY_BIT, .freq = 1300, .freq_lb = 1100, .diff_time = 30};
+Decoder_FSM_Val h_stop   = {.curState = STOP_BIT,   .freq = 1200, .freq_lb = NULL, .diff_time = 30};
+// Decoder_FSM_Val h_idle = {.curState = TRANSMISSION_ERROR, .freq = 2300, .freq_lb = NULL, .diff_time = 300};
 
 sstv_mode_t Robot36  =  {.decMode = ROBOT_36, .color = YCRCB, .row = 320, .col = 240, .tranTime = 36, .lineTime = 135,     .lineS = 10.5,  .colorS = 4.5, .format = 1};
 sstv_mode_t Robot72  =  {.decMode = ROBOT_72, .color = YCRCB, .row = 320, .col = 240, .tranTime = 72, .lineTime = 276,     .lineS = 12.0,  .colorS = 6.0, .format = 2};
